2f9b216855ef35493d326605924b4daaa2a7416d
Kim Sedlacek <sedlaceks@optonline.net>
2023-12-09 16:29:15 -0500

    revised methods exercise with ex2 added

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -5,6 +5,15 @@
         static void Main(string[] args)
         {
             Holidays();
+            Console.WriteLine();
+            Add();
+            Console.WriteLine();
+            Subtract();
+            Console.WriteLine();
+            Multiply();
+            Console.WriteLine();
+            Division();
+
         }
 
         public static void Holidays()
@@ -21,7 +30,7 @@
                 "Carriage Ride\n* Ice Skating\n* Food Trucks\n* Games & Activities\n* Santa visit");
             string favoriteActivity = (Console.ReadLine());
 
-            Console.WriteLine("What is your favorite song that the carolers sang?");
+            Console.WriteLine("Nice, what is your favorite song that the carolers sang?");
             string favoriteSong = Console.ReadLine();
 
             Console.WriteLine("Wonderful! Do you have any suggestions for furture Winter Festivals or other town activities?" +
@@ -30,9 +39,52 @@
 
             Console.WriteLine($"Thank you again {name} for attending the fesival and for participating in this survey. " +
                 $"We are so glad you enjoyed the {favoriteActivity}! We hope to see you next year, and we'll be sure " +
-                $"to have the carolers keep {favoriteSong} on their song list!");
+                $"to have the carolers keep \"{favoriteSong}\" on their song list!");
+
+        }
+
+            public static void Add()
+            {
+                Console.WriteLine("Let's do an additon problem, give me one number:");
+                int add1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Now give me a second number:");
+                int add2 = int.Parse(Console.ReadLine());
+                Console.WriteLine($"{add1} + {add2} = {add1 + add2}");
+
+            }
+
+            public static void Subtract()
+            {
+                Console.WriteLine("Now let's subtract, give me a number:");
+                int sub1 = int.Parse(Console.ReadLine());
+                Console.WriteLine($"And what number do you want to subrtract from {sub1}?");
+                int sub2 = int.Parse(Console.ReadLine());
+                Console.WriteLine($"{sub1} - {sub2} = {sub1 - sub2}");
+            }
+
+            public static void Multiply()
+            {
+                Console.WriteLine("Multiplication, 1st Number:");
+                int mul1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("2nd Number: ");
+                int mul2 = int.Parse(Console.ReadLine());
+                Console.WriteLine($"{mul1 * mul2} is {mul1} x {mul2}");
+
+            }
+
+            public static void Division()
+            {
+                Console.WriteLine("Division-Give me a whole number for the numerator:");
+                int num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Give me another whole number for the denominator");
+                int den2 = int.Parse(Console.ReadLine());
+                Console.WriteLine($"{num1} divided by {den2} is {num1 / den2} or rounds up to {num1 / den2}");
+
+            }
+
 
         }
 
     }
-}
+
+ 
